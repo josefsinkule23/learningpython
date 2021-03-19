7e7ca536c821a7ebf82a161fe7448c2740de8f09
josefsinkule23 <819817@mystma.org>
2021-03-19 15:27:21 +0000

    Mad Lib

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-print("Starwars")
-print("Avengers")
-print("Remember The Titans")
-print("Elf")
-print("Gladiator")
+noun1 ="item shop" 
+noun2 ="fortniters"
+verb1 ="dancing"
+noun3 ="Skrillex"
+adjective1 ="strange"
+adjective2 ="excited"
+print("Today i went to the " + noun1 + ". There were many " + noun2 + " who were " + verb1 + "too " + noun3 ". They all looked very " + adjective1 + ". I was very " + adjective2) 
